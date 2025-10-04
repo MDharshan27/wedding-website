a5337b2c875dcbf1e3d000c46cd17a4c684ebca3
Mohana Dharshan Sandran Kumaran <191207901+MDharshan27@users.noreply.github.com>
2025-10-04 14:09:42 +0530

    Update hero section to stack text on small devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
         <section className="hero">
             <div className="overlay"></div>
             <div className="hero-content">
-                <h1>Vinoth & Vinothini</h1>
+                <h1>Vinoth{"\n"}&{"\n"}Vinothini</h1>
                 <p>27th November 2025</p>
             </div>
         </section>
